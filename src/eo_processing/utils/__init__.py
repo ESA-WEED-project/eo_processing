7bdd555c6150e44e3277ca3ba03e5273d27f9bb5
Hans Vanrompay <hansvanrompay@hotmail.com>
2024-10-10 18:16:29 +0200

    comment out connection in init

--- src/eo_processing/utils/__init__.py
+++ src/eo_processing/utils/__init__.py
@@ -1,7 +1,6 @@
 from .geoprocessing import (laea20km_id_to_extent,
                             reproj_bbox_to_ll,
                             bbox_area)
-#from .helper import init_connection, location_visu, getUDFpath
 
 __all__ = ['laea20km_id_to_extent', 'reproj_bbox_to_ll', 'bbox_area',
            'init_connection', 'location_visu', 'getUDFpath']

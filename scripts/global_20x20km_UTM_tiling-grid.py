067876d55e16cdd75939b87ccb6b6c767ad1545a
Dr. Marcel Buchhorn <marcel.buchhorn@vito.be>
2024-11-19 16:23:39 +0100

    **Update Google Drive access notebook and global UTM tiling script**
    Refactor Google Drive access notebook to streamline credential retrieval and add extensive documentation. Update global UTM tiling script to process and clip grids based on UTM zones and landmasses.

--- scripts/global_20x20km_UTM_tiling-grid.py
+++ scripts/global_20x20km_UTM_tiling-grid.py
@@ -23,3 +23,62 @@ STEPS
 
 '''
 
+import geopandas as gpd
+import pandas as pd
+from shapely.geometry import box
+from shapely.ops import unary_union
+import os
+
+# standard paths
+path_utm = os.path.normpath(r'C:\Users\BUCHHORM\Downloads\global_20x20km_opneEO_processing_grid\global_high-res_UTMzones.gpkg')
+path_land = os.path.normpath(r'C:\Users\BUCHHORM\Downloads\global_20x20km_opneEO_processing_grid\ne_10m_land\Land_masses_10km_buffered_Clipped_S2.gpkg')
+path_grid = os.path.normpath(r'C:\Users\BUCHHORM\Downloads\global_20x20km_opneEO_processing_grid\basic_20x20km_grid_no-crs.gpkg')
+
+# load UTM zones
+gdf_utm = gpd.read_file(path_utm)
+
+# loop over all UTM zones
+lZones = gdf_utm.name.unique().tolist()
+for UTMzone in lZones:
+    print(f'processing zone: {UTMzone}')
+    # get the UTM zone polygon and the EPSG
+    if UTMzone[-1] == 'N':
+        epsg = 32600 + int(UTMzone[:2])
+    else:
+        epsg = 32700 + int(UTMzone[:2])
+
+    gdf_zone = gdf_utm[gdf_utm.name == UTMzone].copy()
+
+    # get the land masses in correct epsg
+    gdf_land = gpd.read_file(path_land)
+    gdf_land = gdf_land.clip(gdf_zone)
+    gdf_land = gdf_land.dissolve()
+
+    # bring all to correct epsg
+    gdf_zone = gdf_zone.to_crs(epsg=epsg)
+    gdf_land = gdf_land.to_crs(epsg=epsg)
+
+    # now we load the basic 20x20km grid and set the crs to the correct epsg
+    gdf_grid = gpd.read_file(path_grid).set_crs(epsg=epsg, allow_override=True)
+
+    # clipping
+    gdf_grid = gdf_grid.clip(gdf_zone)
+    gdf_grid = gdf_grid.clip(gdf_land)
+
+    # name the 20x20 grid cells with the grid20id
+    pass
+
+    # add the bbox dict for openEO
+    pass
+
+    # save to disk
+    gdf_grid.to_file(
+        os.path.normpath(
+            r'C:\Users\BUCHHORM\Downloads\global_20x20km_opneEO_processing_grid\results\UTM_zone_{0}.gpkg'.format(
+                epsg)))
+
+    print()
+
+
+
+print()

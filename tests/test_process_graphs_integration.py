c17dc34a3c152814f77094c18aad21d4c53c4e5f
jeroenwannijn <jeroen.wannijn@vito.be>
2025-07-03 22:32:40 +0200

    fix integration tests

--- tests/test_process_graphs_integration.py
+++ tests/test_process_graphs_integration.py
@@ -14,8 +14,8 @@ INTEGRATION_TESTS = (
         "extract_S2_integration.json": 100, # 7 credits
         "ts_datacube_extraction_combined_integration.json": 228, # 13 credits
         "ts_datacube_extraction_S1_integration.json": 228, # 13 credits       
-        "ts_datacube_extraction_S2_integration.json": 99, # 13 credits
-        "generate_master_feature_cube_with_catboost_inference_integration.json": 132, # 28 credits
+        "ts_datacube_extraction_S2_integration.json": 228, # 13 credits
+        "generate_master_feature_cube_with_catboost_inference_integration.json": 1, # 28 credits
         "generate_S1_feature_cube_integration.json": 1, # 12 credits
         "generate_S2_feature_cube_integration.json": 1, # 15 credits
     }
